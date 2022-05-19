96f7fa57e9c4c695836bbcdc25c097e6b94447da
Doug Beckwith <dougcbeckwith@gmail.com>
2022-05-19 12:53:12 -0700

    add final styles

--- app.js
+++ app.js
@@ -1,26 +1,3 @@
-// APP
-// Display a Timer
-// Shows an animated border around the timer
-
-// Event listener to watch for a click on start button
-// Emit an event starting that the timer as started (Watch
-// for this event. When it occurs, draw the border)
-// Start counting down the timer
-// Emit an event that the timer has ticked (Watch for this event. When
-// it accous, update the border)
-// Each time the timer counts down update the text
-// If we counted down and timer reaches 0
-// Emmit an event that the timer is done (Watch for this event.
-// When it accurs reset the border)
-// Reset internal timer to get ready for another run
-
-// Class timer
-// constructor(durationInput, startButton, pauseButton)
-// Start()
-// Pause()
-// onDurationChange()
-// tick()
-
 const durationInput = document.querySelector('#duration')
 const startButton = document.querySelector('#start')
 const pauseButton = document.querySelector('#pause')
